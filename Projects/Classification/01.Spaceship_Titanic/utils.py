c9e371043636638dd272d4c4916b2687980bf1aa
JuanFe <jmartinezbernal02@gmail.com>
2026-04-05 19:05:05 -0500

    chore ⚒️: remove deprecated utility scripts and remaining classification projects

--- Projects/Classification/01.Spaceship_Titanic/utils.py
+++
@@ -1,7 +0,0 @@
-def calc_detail_cabin(x, position):
-    # Divides la cadena en subcadenas utilizando '/' como separador y devuelve el primer elemento
-    try:
-        return x.split('/')[position]        
-    # Si ocurre algún error al intentar dividir la cadena, se devuelve la cadena original
-    except:
-        return x
